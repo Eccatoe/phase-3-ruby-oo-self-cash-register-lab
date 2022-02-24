a0c54739320191d144708810edeb5e43c9356550
Elizabeth Catoe <eccatoe2517@gmail.com>
2022-02-24 15:20:55 -0500

    commit tree 3421d685d9dfa78e379f27339ddab96f34de7f9e

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
+require 'pry'
 
+class CashRegister
+    
+    attr_accessor :total, :discount, :items
+
+    def initialize (discount=0)
+        @total=0
+        @discount=discount
+        @items=[]
+    end
+
+    def add_item (title, price, quantity=1)
+        self.total+=price*quantity
+        quantity.times do
+            self.items << title
+    end
+
+    def apply_discount
+        if self.discount>0
+        self.total*=(1-discount.to_f*0.01)
+        self.total.to_i
+        "After the discount, the total comes to #{total}"
+        elsif self.discount==0
+        "There is no discount to apply."
+        end
+    end
+end
+
+binding.pry
+0
